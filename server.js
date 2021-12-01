016ba331b7c2c051dae53558ac88614260c416a9
DakoYu <89088363+DakoYu@users.noreply.github.com>
2021-11-30 20:13:45 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
-const https = require("https");
 dotenv.config({path: './config.env'});
 
 const DB = 'mongodb+srv://dako:csolyyj355@cluster0.rqerh.mongodb.net/instagram?retryWrites=true&w=majority';
@@ -16,8 +15,6 @@ mongoose.connect(DB, {
 
 const port = process.env.PORT || 8000;
 
-// app.listen(port, () => {
-//     console.log(`server listening on port ${port}`);
-// });
-
-https.createServer({}, app).listen(8000);
+app.listen(port, () => {
+    console.log(`server listening on port ${port}`);
+});

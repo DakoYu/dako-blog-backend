2b8564bf2266937fc8e70e1eb04bbf6c71d17195
DakoYu <89088363+DakoYu@users.noreply.github.com>
2021-11-30 21:11:44 -0500

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const blogRouter = require('./routes/blogRouter');
 
 app.use(morgan('dev'));
 
-app.use(cors());
+app.use(cors({ origin: 'http://localhost:3000' }));
 
 app.use(express.json({ limit: '10kb' }));
 app.use(cookieParser());
